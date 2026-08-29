6632f8febec41c7c123aad27706e57acf5aa9c13
DESKTOP-0HKF807\pc <237986460+xxhh66@users.noreply.github.com>
2026-08-29 22:48:42 +0800

    注册3,加锁

--- app/services/auth.py
+++ app/services/auth.py
@@ -2,7 +2,7 @@
 
 负责向用户发送邮箱验证码，并限制重发频率，避免恶意刷验证码或重复发送。
 """
-
+import asyncio
 import random
 from datetime import datetime, timedelta
 
@@ -13,30 +13,39 @@ from app.schemas.user import RegisterParam
 from app.utils import smtp_util, pwd_util
 from app.models import User
 
+EMAIL_LOCKS={}
+VERIFY_CODE_LOCK = asyncio.Lock()
+
 class AuthService:
     """认证服务，封装验证码发送等基础认证能力。"""
 
     async def send_verify_code(self, email: str):
         """发送邮箱验证码，并校验是否在冷却期内。"""
-        # 如果同一邮箱最近已发送验证码，则根据创建时间判断是否还在冷却中。
-        verify_code_dict = verify_code_cache.get(email)
-        if verify_code_dict:
-            one_minutes = timedelta(minutes=1)
-            if verify_code_dict.get("created_at") + one_minutes > datetime.now():
-                raise HTTPException(status_code=400, detail="验证码已发送，请稍后再试")
+        if email not in EMAIL_LOCKS:
+            async with VERIFY_CODE_LOCK:
+                if email not in EMAIL_LOCKS:
+                    EMAIL_LOCKS[email] = asyncio.Lock()
 
-        # 生成 6 位随机数字验证码，并保存到缓存，便于后续校验逻辑复用。
-        code = "".join(random.choices("0123456789", k=6))
-        now = datetime.now()
-        verify_code_cache.set(email, {"code": code, "created_at": now})
+        async with EMAIL_LOCKS[email]:
+            # 如果同一邮箱最近已发送验证码，则根据创建时间判断是否还在冷却中。
+            verify_code_dict = verify_code_cache.get(email)
+            if verify_code_dict:
+                one_minutes = timedelta(minutes=1)
+                if verify_code_dict.get("created_at") + one_minutes > datetime.now():
+                    raise HTTPException(status_code=400, detail="验证码已发送，请稍后再试")
 
-        try:
-            # 发送邮件时，消息内容中说明验证码有效时间，便于用户及时填写。
-            smtp_util.send_message(f"您的验证码是：{code},将在3分钟内过期", email, "验证码")
-        except Exception:
-            raise HTTPException(status_code=400, detail="系统繁忙，请稍后重试")
+            # 生成 6 位随机数字验证码，并保存到缓存，便于后续校验逻辑复用。
+            code = "".join(random.choices("0123456789", k=6))
+            now = datetime.now()
+            verify_code_cache.set(email, {"code": code, "created_at": now})
 
-        return True
+            try:
+                # 发送邮件时，消息内容中说明验证码有效时间，便于用户及时填写。
+                smtp_util.send_message(f"您的验证码是：{code},将在3分钟内过期", email, "验证码")
+            except Exception:
+                raise HTTPException(status_code=400, detail="系统繁忙，请稍后重试")
+
+            return True
 
     # 注册
     async def register(self, param: RegisterParam):
