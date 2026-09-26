00ce0c906ea39812345f30433ee54edcd76fa5ca
DESKTOP-0HKF807\pc <237986460+xxhh66@users.noreply.github.com>
2026-09-26 16:39:01 +0800

    缓存修改，页面最新文章

--- app/cache/articles.py
+++ app/cache/articles.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
-
-from app.core.caches import load_cache, article_cache
+from typing import List
+from app.core.caches import load_cache, article_cache, article_page_cache
 from app.core.enums import ArticleStatusEnum
 from app.models import Article
+from app.schemas.admin.articles import ArticlePageItemResult
 from app.schemas.articles import ArticlePydantic
+from app.schemas.common import ApiPageResult
 
 
 # 涉及业务的放在cache/articles下面
@@ -18,3 +20,21 @@ class ArticleCacheService:
             return ArticlePydantic.model_validate(_article).model_dump()
 
         return await load_cache(article_cache_key,article_cache,_fetch_from_db)
+
+    async def page_list(self,cache_key:str,page:int,page_size:int)->ApiPageResult[List[ArticlePageItemResult]]:
+        result = article_page_cache.get(cache_key)
+        if  result is None:
+            return None
+        count = result.get('count')
+        result_list = []
+
+        if count>0:
+            ids = result.get('ids')
+            for article_id in ids:
+                article = await self.get_article_by_id(article_id)
+                if article and article.get('status') == ArticleStatusEnum.PUBLISHED:
+                    result_list.append(ArticlePageItemResult.model_validate(article))
+        return ApiPageResult.success(page,page_size,count,result_list)
+
+    async def page_list_set(self, cache_key, data:dict):
+        article_page_cache.set(cache_key, data)
--- app/services/articles.py
+++ app/services/articles.py
@@ -19,32 +19,60 @@ class ArticleService:
     def __init__(self,article_cache_service:ArticleCacheService):
         self.article_cache_service = article_cache_service
 
+    # async def page_latest_articles(self, param: BasePageParam) -> ApiPageResult[List[ArticlePageItemResult]]:
+    #     # 先从缓存中获取,但会存在如果页码不一样，会在多个地方进行缓存，导致重复缓存，如果数据进行更新， 没办法处理
+    #     cache_key = f'latest:{param.page}:{param.page_size}'
+    #     result = latest_articles_cache.get(cache_key)
+    #
+    #     if result:
+    #         return result
+    #
+    #     async with LATEST_ARTICLES_LOCK:
+    #         result = latest_articles_cache.get(cache_key)
+    #         if result:
+    #             return result
+    #         # 缓存中没有, 从db中获取
+    #         queryset = Article.filter(is_deleted=False, status=ArticleStatusEnum.PUBLISHED).prefetch_related('category', 'tags').order_by('-id')
+    #         count = await queryset.count()
+    #         articles = []
+    #         if count > 0:
+    #             articles = await queryset.offset((param.page - 1) * param.page_size).limit(param.page_size).all()
+    #         result_list = [ArticlePageItemResult.model_validate(article) for article in articles]
+    #
+    #         # 将结果放入缓存
+    #         result = ApiPageResult.success(param.page, param.page_size, count, result_list)
+    #         latest_articles_cache.set(cache_key,result)
+    #
+    #     return result
     async def page_latest_articles(self, param: BasePageParam) -> ApiPageResult[List[ArticlePageItemResult]]:
-        # 先从缓存中获取,但会存在如果页码不一样，会在多个地方进行缓存，导致重复缓存，如果数据进行更新， 没办法处理
+        # 先从缓存中获取
         cache_key = f'latest:{param.page}:{param.page_size}'
-        result = latest_articles_cache.get(cache_key)
+        result = await self.article_cache_service.page_list(cache_key,param.page,param.page_size)
 
         if result:
             return result
 
-        async with LATEST_ARTICLES_LOCK:
-            result = latest_articles_cache.get(cache_key)
-            if result:
-                return result
-            # 缓存中没有, 从db中获取
-            queryset = Article.filter(is_deleted=False, status=ArticleStatusEnum.PUBLISHED).prefetch_related('category', 'tags').order_by('-id')
-            count = await queryset.count()
-            articles = []
-            if count > 0:
-                articles = await queryset.offset((param.page - 1) * param.page_size).limit(param.page_size).all()
-            result_list = [ArticlePageItemResult.model_validate(article) for article in articles]
 
-            # 将结果放入缓存
-            result = ApiPageResult.success(param.page, param.page_size, count, result_list)
-            latest_articles_cache.set(cache_key,result)
+        # 缓存中没有, 从db中获取
+        queryset = (Article.filter(is_deleted=False, status=ArticleStatusEnum.PUBLISHED)
+                    .prefetch_related('category', 'tags')
+                    .order_by('-id'))
+        count = await queryset.count()
+        articles = []
+        if count > 0:
+            articles = await queryset.offset((param.page - 1) * param.page_size).limit(param.page_size).all()
+        result_list = [ArticlePageItemResult.model_validate(article) for article in articles]
+
+        ids = [article.id for article in articles]
+
+        result = ApiPageResult.success(param.page, param.page_size, count, result_list)
+        # 将结果放入缓存
+        await self.article_cache_service.page_list_set(cache_key,{
+            'count':count,
+            'ids':ids,
+        })
 
         return result
-
     # async def get_by_id(self,article_id:int)->ArticleDetailResult:
     #     article_lock_key = f'article:{article_id}'
     #     article_lock = article_lock_cache.get(article_lock_key)
